53a62891d797f5e7fba8c1e95210f84be2452bb6
TobyMayn <tobiasmayn@gmail.com>
2024-06-05 18:07:28 +0200

    create hashset file

---
+++ data-structures/hashmaps/hashset.c
@@ -0,0 +1,37 @@
+typedef struct {
+    int key;
+    signed int value;
+} MyHashSet;
+
+
+MyHashSet* myHashSetCreate() {
+    
+}
+
+void myHashSetAdd(MyHashSet* obj, int key) {
+    
+}
+
+void myHashSetRemove(MyHashSet* obj, int key) {
+    
+}
+
+bool myHashSetContains(MyHashSet* obj, int key) {
+    
+}
+
+void myHashSetFree(MyHashSet* obj) {
+    
+}
+
+/**
+ * Your MyHashSet struct will be instantiated and called as such:
+ * MyHashSet* obj = myHashSetCreate();
+ * myHashSetAdd(obj, key);
+ 
+ * myHashSetRemove(obj, key);
+ 
+ * bool param_3 = myHashSetContains(obj, key);
+ 
+ * myHashSetFree(obj);
+*/
